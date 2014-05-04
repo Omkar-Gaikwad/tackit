95c8ee0e3c76d35cff72f1e8cc3f3ca397da9307
omkar-gaikwad <omkgkwd@gmail.com>
2014-05-03 20:09:12 -0700

    here is my commit with test

--- tackit/src/main/java/com/tackit/domain/DashBoard.java
+++ tackit/src/main/java/com/tackit/domain/DashBoard.java
@@ -9,37 +9,26 @@ public class DashBoard {
 	ArrayList<User> followerlist;
 	ArrayList<Tacks> tacks;
 	
-	
 
 	public String getTitle() {
 		return title;
 	}
 
-
-
 	public void setTitle(String title) {
 		this.title = title;
 	}
-
-
 
 	public String getOwner() {
 		return owner;
 	}
 
-
-
 	public void setOwner(String owner) {
 		this.owner = owner;
 	}
 
-
-
 	public ArrayList<User> getFollowerlist() {
 		return followerlist;
 	}
-
-
 
 	public void setFollowerlist(ArrayList<User> followerlist) {
 		this.followerlist = followerlist;
@@ -56,17 +45,13 @@ public class DashBoard {
 		
 	}
 
-
 	public ArrayList<Tacks> getTacks() {
 		return tacks;
 	}
 
-
-
 	public void setTacks(ArrayList<Tacks> tacks) {
 		this.tacks = tacks;
 	}
-
 
 
 	public void addTacks( Tacks tack) {
---
+++ tackit/src/main/java/com/tackit/domain/myclass.java
@@ -0,0 +1,5 @@
+package com.tackit.domain;
+
+public class myclass {
+
+}
